a1d7c6c9e44dbbf88925c72dd1c119bcb59b4367
Eric Scott <scottericr@gmail.com>
2024-10-28 17:00:02 -0700

    add test using gistr

--- tests/testthat/test-gitcreds.R
+++ tests/testthat/test-gitcreds.R
@@ -1,3 +1,4 @@
 test_that("PAT gets found", {
   expect_equal(gitcreds::gitcreds_get()$password, Sys.getenv("GITHUB_PAT"))
+  expect_equal(gistr::gist_auth()$Authorization, paste0("token ", Sys.getenv("GITHUB_PAT")))
 })
